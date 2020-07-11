cad518d11c9c752367756e1c58072497ba4cb33c
Joel070 <joelpele2003@gmail.com>
2020-07-11 13:47:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-akyhfr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-akyhfr)
---
+++ index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+class Contador extends Component{
+
+constructor(props){
+  super(props);
+
+  this.state = {
+    contador: 0
+  };
+}
+
+aumentar = ()=>{
+  this.setState({
+    contador: this.state.contador + 1
+  })
+}
+
+  render(){
+    return (<div>
+    <p>{this.state.contador}</p>
+    <button onClick={ this.aumentar  }>ClickMe!</button>
+    </div>);
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    let nombre = "Joelito";  
+    return (
+      <div>
+        <Contador />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
